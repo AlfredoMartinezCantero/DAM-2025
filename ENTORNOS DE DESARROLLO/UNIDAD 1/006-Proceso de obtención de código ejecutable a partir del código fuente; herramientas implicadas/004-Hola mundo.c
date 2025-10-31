f2974f102b7c007b2f91fa12be40d449e1bfda05
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-10-31 09:26:50 +0100

    1

---
+++ ENTORNOS DE DESARROLLO/UNIDAD 1/006-Proceso de obtención de código ejecutable a partir del código fuente; herramientas implicadas/004-Hola mundo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// Include = Traigo librería
+// std = standard (la de por defecto)
+// i = input (entrada)
+// o = output (salida)
+// .h indica librería de C
+
+int main(){
+    printf("Hola mundo desde C");
+    return 0;
+}    
+// Main es la función principal
+// int es el tipo de datos que devuelve la función
+// return 0 cumple la promesa de devolver un entero (0)
+// printf equivale a print
+// En C las instrucciones terminan obligatoriamente en ;
---
+++ ENTORNOS DE DESARROLLO/UNIDAD 1/006-Proceso de obtención de código ejecutable a partir del código fuente; herramientas implicadas/007-Hola mundo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// Include = Traigo librería
+// i = input (entrada)
+// o = output (salida)
+// stream = flujo de datos
+
+using namespace std;
+// Uso un espacio de nombres
+
+int main(){
+    cout << "Hola mundo desde C++";
+}
+
+// compilar: g++ 007-Hola\ mundo.cpp -o acpp.out (En la máquina virtual)
