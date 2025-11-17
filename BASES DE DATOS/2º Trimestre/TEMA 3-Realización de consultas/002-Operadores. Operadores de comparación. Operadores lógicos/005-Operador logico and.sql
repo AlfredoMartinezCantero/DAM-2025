094bd609baa490afd097a27c4298b776deaf0985
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-11-17 10:58:53 +0100

    1

--- BASES DE DATOS/2º Trimestre/TEMA 3-Realización de consultas/002-Operadores. Operadores de comparación. Operadores lógicos/005-Operador logico and.sql
+++ BASES DE DATOS/2º Trimestre/TEMA 3-Realización de consultas/002-Operadores. Operadores de comparación. Operadores lógicos/005-Operador logico and.sql
@@ -5,6 +5,6 @@ nombre,
 apellidos,
 edad,
 edad < 30 AS 'Menor de 30 años',
-edad >= 30 && < 40 AS 'Entre 30 y 40 años',
+edad >= 30 && edad < 40 AS 'Entre 30 y 40 años',
 edad > 40 AS 'Mayor de 40 años'
 FROM clientes;
---
+++ BASES DE DATOS/2º Trimestre/TEMA 3-Realización de consultas/003-Consultas de resumen/009-Graficas.py
@@ -0,0 +1,5 @@
+# pip install matplotlib
+import matplotlib.pyplot as pt
+
+pt.pie([40, 30, 20, 10])
+pt.show()
