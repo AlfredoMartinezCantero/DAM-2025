214a5786bcd9b35ef14b75cfff88bc0b37ae7343
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-11-19 09:21:00 +0100

    1

---
+++ PROGRAMACION/2º Trimestre/TEMA 6-Aplicación de las estructuras de almacenamiento/003-Matrices multidimensionales/013-Agenda.py
@@ -0,0 +1,9 @@
+agenda = []
+
+while True:
+    nombre = input("Dime tu nombre: ")
+    apellidos = input("Dime tus apellidos: ")
+    email = input("Dime tu email: ")
+    telefono = input("Dime tu número de teléfono: ")
+    # Añado a la agenda
+    agenda.append([nombre,apellidos,email,telefono])
