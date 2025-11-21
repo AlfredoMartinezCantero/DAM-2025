4c343a5638c0ef5299af4262ac9c80efe3cbec99
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-11-21 10:21:11 +0100

    1

---
+++ PROYECTO/2º Trimestre/103-Jinja/007-paso varias cosas.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+import mysql.connector
+
+######################################### MYSQL ##########################################################
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="usuario_portafolio",  
+    password="mi_contraseña_segura",
+    database="portafolio"
+)                                       # Datos de conexión a la BBDD
+cursor = conexion.cursor()              # Creo un cursor MySQL
+# ---------------------- ESTO ENVÍA LAS TABLAS ------------------------------------------------------- #
+cursor.execute("SHOW TABLES;")          # Muestra las tablas de la base de datos
+tablas = []                             # Creo una lista vacia
+filas = cursor.fetchall()               # Lo guardo en una lista
+for fila in filas:                      # Recorro el resultado
+    tablas.append(fila[0])              # Lo añado a la lista de tablas
+# ---------------------- ESTO ENVÍA LAS CABECERAS DE LAS TABLAS -------------------------------------- #
+cursor.execute("SHOW COLUMNS in Piezas;")          
+columnas = []                             
+filas = cursor.fetchall()               
+for fila in filas:                      
+    columnas.append(fila[0])              
+######################################### MYSQL ##########################################################
+
+app = Flask(__name__)
+
+@app.route("/")                 
+def inicio():   
+    return render_template(
+        "backoffice.html",
+        mis_tablas = tablas,
+        mis_columnas = columnas
+        )                        # Envío las tablas a HTML
+
+if __name__ == "__main__":
+    app.run(debug=True)
