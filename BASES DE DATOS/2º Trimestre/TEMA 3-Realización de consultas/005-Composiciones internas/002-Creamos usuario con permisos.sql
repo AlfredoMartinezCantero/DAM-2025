709bd11041ba18dce5dcf7778bf0d89b3a808b01
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-11-24 10:38:31 +0100

    1

--- BASES DE DATOS/2º Trimestre/TEMA 3-Realización de consultas/005-Composiciones internas/002-Creamos usuario con permisos.sql
+++ BASES DE DATOS/2º Trimestre/TEMA 3-Realización de consultas/005-Composiciones internas/002-Creamos usuario con permisos.sql
@@ -1,16 +1,16 @@
 -- crea usuario nuevo con contraseña
 -- creamos el nombre de usuario que queramos
 CREATE USER 
-'tiendaclase'@'localhost' 
-IDENTIFIED  BY '[tucontraseña]';
+'tienda'@'localhost' 
+IDENTIFIED  BY 'Tiendaclase123$';
 
 -- permite acceso a ese usuario
-GRANT USAGE ON *.* TO 'tiendaclase'@'localhost';
+GRANT USAGE ON *.* TO 'tienda'@'localhost';
 --[tuservidor] == localhost
 -- La contraseña puede requerir Mayus, minus, numeros, caracteres, min len
 
 -- quitale todos los limites que tenga
-ALTER USER 'tiendaclase'@'localhost' 
+ALTER USER 'tienda'@'localhost' 
 REQUIRE NONE 
 WITH MAX_QUERIES_PER_HOUR 0 
 MAX_CONNECTIONS_PER_HOUR 0 
@@ -18,8 +18,8 @@ MAX_UPDATES_PER_HOUR 0
 MAX_USER_CONNECTIONS 0;
 
 -- dale acceso a la base de datos empresadam
-GRANT ALL PRIVILEGES ON [tubasededatos].* 
-TO 'tiendaclase'@'localhost';
+GRANT ALL PRIVILEGES ON tienda.* 
+TO 'tienda'@'localhost';
 
 -- recarga la tabla de privilegios
 FLUSH PRIVILEGES;
