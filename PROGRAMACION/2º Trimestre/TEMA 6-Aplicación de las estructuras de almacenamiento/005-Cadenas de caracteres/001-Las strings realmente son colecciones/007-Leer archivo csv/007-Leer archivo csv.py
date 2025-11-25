3b90713cfb8bdb98043069420e0c656f97ed5125
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-11-25 11:07:46 +0100

    1

--- PROGRAMACION/2º Trimestre/TEMA 6-Aplicación de las estructuras de almacenamiento/005-Cadenas de caracteres/001-Las strings realmente son colecciones/007-Leer archivo csv/007-Leer archivo csv.py
+++ PROGRAMACION/2º Trimestre/TEMA 6-Aplicación de las estructuras de almacenamiento/005-Cadenas de caracteres/001-Las strings realmente son colecciones/007-Leer archivo csv/007-Leer archivo csv.py
@@ -1,4 +1,4 @@
-archivo = open("clientes.csv","r")
+archivo = open(r"C:\Users\alfre\Documents\GitHub\DAM-2025\PROGRAMACION\2º Trimestre\TEMA 6-Aplicación de las estructuras de almacenamiento\005-Cadenas de caracteres\001-Las strings realmente son colecciones\007-Leer archivo csv\clientes.csv", "r")
 
 lineas = archivo.readlines()
 
