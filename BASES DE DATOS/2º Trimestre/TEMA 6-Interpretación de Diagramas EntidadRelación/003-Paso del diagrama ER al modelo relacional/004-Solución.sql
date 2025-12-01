77164fee74815b664d572f66a5fbd34aec971af4
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-12-01 10:52:50 +0100

    1

---
+++ BASES DE DATOS/2º Trimestre/TEMA 6-Interpretación de Diagramas EntidadRelación/003-Paso del diagrama ER al modelo relacional/004-Solución.sql
@@ -0,0 +1,9 @@
+-- La solución recomendada es unir tabla
+CREATE TABLE Clientes(
+    id INT PRIMARY KEY AUTO_INCREMENTAL,
+    nombre VARCHAR(255),
+    apellidos VARCHAR(255),
+    email VARCHAR(255),
+    direccion VARCHAR(255),
+    DNINIE VARCHAR(255)
+);
