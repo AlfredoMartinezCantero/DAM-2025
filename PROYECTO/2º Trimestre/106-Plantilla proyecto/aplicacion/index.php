a6b693b78fd4949dbc4b62acf4a312057933c28e
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-12-09 13:19:57 +0100

    1

--- PROYECTO/2º Trimestre/106-Plantilla proyecto/aplicacion/index.php
+++ PROYECTO/2º Trimestre/106-Plantilla proyecto/aplicacion/index.php
@@ -1,15 +1,35 @@
 <!doctype html>
 <html lang="es">
 	<head>
-        <title>El AMC - Noticias tecnológicas</title>
-        <meta charset="utf-8">
+  	<title>El AAMC - Noticias tecnológicas</title>
+    <meta charset="utf-8">
   </head>
   <body>
   	<header>
-    	<h1>El AMC</h1>
+    	<h1>El AAMC</h1>
       <h2>Noticias tecnológicas</h2>
     </header>
     <main>
+    	<?php
+        $host = "localhost";
+        $user = "periodico";
+        $pass = "Periodico123$";
+        $db   = "periodico";
+        $conexion = new mysqli($host, $user, $pass, $db);
+        $sql = "SELECT * FROM noticias";
+        $resultado = $conexion->query($sql);
+        while ($fila = $resultado->fetch_assoc()) {
+          echo '
+            <article>
+              <h3>'.$fila['titulo'].'</h3>
+              <time>'.$fila['fecha_publicacion'].'</time>
+              <p>'.$fila['autor_id'].'</p>
+              <p>'.$fila['contenido'].'</p>
+            </article>
+          ';
+        }
+        $conexion->close();
+      ?>
     </main>
     <footer>
     </footer>
