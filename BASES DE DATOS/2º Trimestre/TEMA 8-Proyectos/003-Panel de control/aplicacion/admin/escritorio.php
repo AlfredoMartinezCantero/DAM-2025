5df75b0ec0cc874a25cfc80efe19922ab04eb49f
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-12-10 14:10:09 +0100

    10/12/025

--- BASES DE DATOS/2º Trimestre/TEMA 8-Proyectos/003-Panel de control/aplicacion/admin/escritorio.php
+++ BASES DE DATOS/2º Trimestre/TEMA 8-Proyectos/003-Panel de control/aplicacion/admin/escritorio.php
@@ -11,6 +11,24 @@
       <button>Autores</button>
     </nav>
     <main>
+    	<table>
+      	<thead>
+        	<tr>
+          	<th>Titulo</th><th>Texto</th><th>Fecha</th><th>Autor</th><th>Categoria</th>
+          </tr>
+        </thead>
+        <tbody>
+        	<?php
+          	for($i = 0;$i<20;$i++){
+            	echo '
+                <tr>
+                  <td>Titulo</td><td>Texto</td><td>Fecha</td><td>Autor</td><td>Categoria</td>
+                </tr>
+              	';
+            }
+          ?>
+        </tbody>
+      </table>
     </main>
   </body>
 </html>
