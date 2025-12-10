71aede85a44632f8d0586f022c4d8b35959a15a6
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-12-10 14:02:02 +0100

    10/12/025

---
+++ BASES DE DATOS/2º Trimestre/TEMA 8-Proyectos/003-Panel de control/aplicacion/admin/escritorio.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="es">
+	<head>
+  	<title>El AAMC - Panel de control</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/estilo.css">
+  </head>
+  <body>
+  	<nav>
+    	<button>Noticias</button>
+      <button>Autores</button>
+    </nav>
+    <main>
+    </main>
+  </body>
+</html>
