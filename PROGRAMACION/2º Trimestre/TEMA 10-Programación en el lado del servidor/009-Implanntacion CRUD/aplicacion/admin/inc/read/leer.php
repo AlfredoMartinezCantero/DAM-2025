bf50dc31a09e7f27c952bb63a3d0c4bc31368e18
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-12-11 10:57:26 +0100

    Update leer.php

--- PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/009-Implanntacion CRUD/aplicacion/admin/inc/read/leer.php
+++ PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/009-Implanntacion CRUD/aplicacion/admin/inc/read/leer.php
@@ -1,23 +1,27 @@
-<?php
-	session_start(); 
-  $host = "localhost";
-  $user = "periodico";
-  $pass = "Periodico123$";
-  $db   = "periodico";
+<table>    
+    <?php
+        session_start(); 
+    $host = "localhost";
+    $user = "periodico";
+    $pass = "Periodico123$";
+    $db   = "periodico";
 
-  $conexion = new mysqli($host, $user, $pass, $db);
-  
-	
-  $sql = "
-  	SELECT * FROM noticias;
-  ";
-	
-  $resultado = $conexion->query($sql);
-  while ($fila = $resultado->fetch_assoc()) {
-  	var_dump($fila);			
-  }
-	
-  	
-  $conexion->close();
-  
-?>
+    $conexion = new mysqli($host, $user, $pass, $db);
+       
+    $sql = "
+        SELECT * FROM noticias;";
+        
+    $resultado = $conexion->query($sql);
+    while ($fila = $resultado->fetch_assoc()) {
+        echo "<tr>";
+            echo "<td>".$fila['titulo']."</td>";
+            echo "<td>".$fila['fecha_publicacion']."</td>";
+            echo "<td>".$fila['autor_id']."</td>";
+            echo "<td>".$fila['contenido']."</td>";
+        echo "</tr>";
+        var_dump($fila);			
+    }
+        
+        $conexion->close();
+    ?>
+</table>
