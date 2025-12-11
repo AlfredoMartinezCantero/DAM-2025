2ad7146c3f66de8757c6a555f191f031d567aa8c
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-12-11 12:10:15 +0100

    Update formulario.php

--- PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/009-Implantacion CRUD/aplicacion/admin/inc/create/formulario.php
+++ PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/009-Implantacion CRUD/aplicacion/admin/inc/create/formulario.php
@@ -1,1 +1,17 @@
-Aqui pondré el formulario de inserción
+<form action="procesaformulario.php" method="POST">
+
+	<label for="titulo">Título de la nueva noticia</label>
+	<input type="text" name="titulo" id="titulo">
+  
+    <label for="contenido">Contenido de la nueva noticia</label>
+	<textarea id="contenido" name="contenido"></textarea>
+  
+    <label for="fecha_publicacion">Fecha de la nueva noticia</label>
+	<input type="text" name="fecha_publicacion" id="fecha_publicacion">
+  
+    <label for="autor_id">Autor de la nueva noticia</label>
+	<input type="text" name="autor_id" id="autor_id">
+  
+    <input type="submit">
+    
+</form>
