7b5926c2bd9a4690e1574922fe29081917e6874a
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-12-12 09:01:00 +0100

    12/12/025

--- PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/009-Implantacion CRUD/aplicacion/admin/inc/read/leer.php
+++ PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/009-Implantacion CRUD/aplicacion/admin/inc/read/leer.php
@@ -1,27 +1,25 @@
-<table>    
-    <?php
-        session_start(); 
+<table>
+  <?php
     $host = "localhost";
     $user = "periodico";
     $pass = "Periodico123$";
     $db   = "periodico";
 
     $conexion = new mysqli($host, $user, $pass, $db);
-       
-    $sql = "
-        SELECT * FROM noticias;";
-        
+
+    $sql = "SELECT * FROM noticias;";
+
     $resultado = $conexion->query($sql);
     while ($fila = $resultado->fetch_assoc()) {
-        echo "<tr>";
-            echo "<td>".$fila['titulo']."</td>";
-            echo "<td>".$fila['fecha_publicacion']."</td>";
-            echo "<td>".$fila['autor_id']."</td>";
-            echo "<td>".$fila['contenido']."</td>";
-        echo "</tr>";
-        			
+			echo "<tr>";
+      	echo "<td>".$fila['titulo']."</td>";
+        echo "<td>".$fila['fecha_publicacion']."</td>";
+        echo "<td>".$fila['autor_id']."</td>";
+        echo "<td>".$fila['contenido']."</td>";
+        echo "<td><a href='?accion=eliminar&id=".$fila['id']."' class='eliminar'>X</a></td>"; // NUEVO ///////////////
+      echo "</tr>";
     }
-        
-        $conexion->close();
-    ?>
+
+    $conexion->close();
+  ?>
 </table>
