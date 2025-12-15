a83a7e05c1d8cd3da08878e418ebe4a00fca4b69
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-12-15 08:55:14 +0100

    Update carrito.php

--- PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/010-Tienda Online/005-Vamos al carrito/front/carrito.php
+++ PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/010-Tienda Online/005-Vamos al carrito/front/carrito.php
@@ -7,4 +7,22 @@ Vamos a ver si atrapamos correctamente el producto<br>
   echo "Las unidades son: ".$_POST['unidades']."<br>";
 ?>
 
+<form method="POST" action="finalizar.php">
+    <!-- Datos de los productos -->
+     <input type="hidden" name="idproducto" value="<?= $_POST['id'] ?>">
+     <input type="hidden" name="unidades" value="<?= $_POST['unidades'] ?>">
+    
+    <!-- Datos del cliente -->
+    <input type="text" name="nombre_cliente">
+    <input type="text" name="apellidos">
+    <input type="text" name="email">
+    <input type="text" name="direccion">
+    <input type="text" name="telefono">
+
+    <!-- Y enviamos -->
+    <input type="submit">
+
+</form>
+
+
 <?php include "inc/piedepagina.php"; ?>
