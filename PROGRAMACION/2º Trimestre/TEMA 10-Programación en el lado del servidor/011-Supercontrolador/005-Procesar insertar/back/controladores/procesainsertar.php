706bf4863fd4ba44ec14b1610300f2e70d661567
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-12-17 10:08:34 +0100

    Update procesainsertar.php

--- PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/011-Supercontrolador/005-Procesar insertar/back/controladores/procesainsertar.php
+++ PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/011-Supercontrolador/005-Procesar insertar/back/controladores/procesainsertar.php
@@ -1,1 +1,24 @@
-Ok que sepas que voy a insertar el formulario
+<!doctype html>
+<html>
+    <head>
+  	<link rel="stylesheet" href="css/estilo.css">
+  </head>
+  <body>
+		<?php include "inc/conexion_bd.php"; ?>
+    <nav>
+    	<?php include "controladores/poblar_menu.php" ?>
+    </nav>
+    <main>
+
+<?php
+    $sql = "INSERT INTO ".$_GET['tabla']." VALUES ";
+    foreach($_POST as $clave=>$valor){
+        if($clave == "id"){
+            $sql.= "NULL,";
+        }else{
+            $sql.="'".$valor."',";
+        }
+  }
+  $sql = substr($sql, 0, -1);       // Le quito la última coma al SQL
+  echo $sql;
+?>
