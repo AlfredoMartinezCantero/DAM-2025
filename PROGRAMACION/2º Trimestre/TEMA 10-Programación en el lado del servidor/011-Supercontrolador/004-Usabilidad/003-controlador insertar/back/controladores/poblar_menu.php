db9399448f3ca193d112d16b384570fe0771654e
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-12-17 09:44:13 +0100

    Update poblar_menu.php

--- PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/011-Supercontrolador/004-Usabilidad/003-controlador insertar/back/controladores/poblar_menu.php
+++ PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/011-Supercontrolador/004-Usabilidad/003-controlador insertar/back/controladores/poblar_menu.php
@@ -4,9 +4,11 @@
    SHOW TABLES;
   ");
   while ($fila = $resultado->fetch_assoc()) {
-  	$clase = "";							                        // De entrada no tienes clase	
-    if($fila['Tables_in_'.$db] == $_GET['tabla']){	  // Pero si el nombre de esta tabla coincide con la tabla cargada
-    	$clase  = "activo";			                        // En ese caso tu clase es "activo"
+  	$clase = "";							                            // De entrada no tienes clase	
+    if(isset($_GET['tabla'])){
+    	if($fila['Tables_in_'.$db] == $_GET['tabla']){	    // Pero si el nombre de esta tabla coincide con la tabla cargada
+    		$clase  = "activo";			                          // En ese caso tu clase es "activo"
+    	}
     }
      echo '
      	<a href="?tabla='.$fila['Tables_in_'.$db].'" class="'.$clase.'">
@@ -15,11 +17,8 @@
       ';
   }
 ?>
-
 <style>
-  .activo{
-    background:red;
+	.activo{
   	transform:translateX(20px);
   }
-
 </style>
