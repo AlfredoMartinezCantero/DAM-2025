aa20643688aedea17a343560fae501cbf8442dd4
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-12-17 08:21:09 +0100

    Create 015-foreach en objeto.php

---
+++ PROGRAMACION/2º Trimestre/TEMA 7-Utilización avanzada de clases/005-Interfaces/015-foreach en objeto.php
@@ -0,0 +1,13 @@
+<?php
+	
+    $cliente = [
+  	  "nombre" => "Jose Vicente",
+      "apellidos" => "Carratala Sanchis",
+      "email" => "info@jocarsa.com"
+    ];
+  
+    foreach($cliente as $clave=>$valor){
+  	  echo $clave.": ".$valor."<br>";
+    }
+ 
+?>
