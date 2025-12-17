3b24c9c3c89871c7e74c2e171bc45f4bed565c71
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2025-12-17 09:29:04 +0100

    Update insertar.php

--- PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/011-Supercontrolador/003-controlador insertar/back/controladores/insertar.php
+++ PROGRAMACION/2º Trimestre/TEMA 10-Programación en el lado del servidor/011-Supercontrolador/003-controlador insertar/back/controladores/insertar.php
@@ -1,1 +1,22 @@
-Parece que quieres insertar algo
+<form action="" method="POST">
+<?php
+      // Creamos un formulario dinámico
+      
+        $resultado = $conexion->query("
+          SELECT * FROM ".$_GET['tabla']." LIMIT 1;
+        ");	                        // SOLO QUIERO UN ELEMENTO ////
+        while ($fila = $resultado->fetch_assoc()) {
+          foreach($fila as $clave=>$valor){
+            echo "
+            	<div class='control_formulario'>
+                <label>".$clave."</label>
+                <input 
+                  type='text' 
+                  name='".$clave."'
+                  placeholder='".$clave."'>
+              </div>
+              ";
+          }
+         }
+      ?>
+</form>
