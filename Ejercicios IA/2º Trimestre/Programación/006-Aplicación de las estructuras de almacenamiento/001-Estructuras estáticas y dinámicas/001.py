167dfacff784ba372ce86469e3e15beaab020aac
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2026-01-06 19:21:50 +0100

    1

---
+++ Ejercicios IA/2º Trimestre/Programación/006-Aplicación de las estructuras de almacenamiento/001-Estructuras estáticas y dinámicas/001.py
@@ -0,0 +1,12 @@
+lista_canciones = ["Desafíate",
+                   "Eres un lobo",
+                   "La muerte es solo el final del comienzo"
+                   ]
+
+print(lista_canciones)
+
+print(lista_canciones[0])
+
+lista_canciones[1] = "La ciudad de los sueños"
+
+print(lista_canciones)
