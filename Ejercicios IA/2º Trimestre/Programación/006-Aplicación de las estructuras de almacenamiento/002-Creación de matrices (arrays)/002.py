fab6938e34ba5b52f02728906063e5dbdb2cfc54
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2026-01-06 19:53:27 +0100

    Create 002.py

---
+++ Ejercicios IA/2º Trimestre/Programación/006-Aplicación de las estructuras de almacenamiento/002-Creación de matrices (arrays)/002.py
@@ -0,0 +1,31 @@
+'''
+Enunciado paso a paso
+Crear una lista de comidas:
+
+Crea una lista vacía llamada menu.
+Implementa un bucle infinito que permita al usuario introducir nuevas comidas en la lista.
+
+Añade las comidas a la lista utilizando el método append().
+
+
+Listar comidas:
+
+Dentro del mismo bucle, agrega una opción para listar todas las comidas que se han añadido hasta ahora.
+
+Utiliza un bucle for para imprimir cada comida en la lista.
+
+Guardar comidas en archivo:
+
+Agrega una opción al menú para guardar la lista de comidas en un archivo binario.
+
+Usa el módulo pickle para serializar la lista y guardarlo en un archivo llamado datos.bin.
+
+Cargar comidas desde archivo:
+
+Añade una nueva opción que permita cargar las comidas desde el archivo datos.bin.
+Utiliza pickle.load() para deserializar la lista y cargarla en tu programa.
+Restricciones
+Solo puedes usar los conceptos y funciones que se han visto hasta ahora.
+No puedes usar librerías externas ni estructuras de datos más complejas.
+Asegúrate de manejar correctamente el modo de apertura del archivo (wb para escritura binaria).
+'''
