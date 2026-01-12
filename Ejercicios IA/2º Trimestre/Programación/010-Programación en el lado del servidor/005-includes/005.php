b339adb51a8b0d083f75ede8cf994544bb4dcc30
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2026-01-12 16:30:14 +0100

    12/01/2026

---
+++ Ejercicios IA/2º Trimestre/Programación/010-Programación en el lado del servidor/005-includes/005.php
@@ -0,0 +1,3 @@
+<?php include "bloques/cabeceraa.php"; ?>
+<p> Me encanta escuchar música, especialmente del género extremoduro, y disfruto jugando videojuegos, especialmente shooters como Halo y Souls</p>
+<?php include "bloques/pie.php";  ?>
