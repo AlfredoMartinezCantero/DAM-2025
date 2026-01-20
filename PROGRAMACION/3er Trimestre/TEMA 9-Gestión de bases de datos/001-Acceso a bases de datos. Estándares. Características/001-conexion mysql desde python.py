26fc17c21cb033f64bbfb0a74baa9d3bb3eba2db
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2026-01-20 10:50:20 +0100

    1

--- PROGRAMACION/3er Trimestre/TEMA 9-Gestión de bases de datos/001-Acceso a bases de datos. Estándares. Características/001-conexion mysql desde python.py
+++ PROGRAMACION/3er Trimestre/TEMA 9-Gestión de bases de datos/001-Acceso a bases de datos. Estándares. Características/001-conexion mysql desde python.py
@@ -1,18 +1,18 @@
-# pip3 install pymysql
-import pymysql
+import mysql.connector
 
-conn = pymysql.connect(
+conn = mysql.connector.connect(
     host="localhost",
     user="empresa2026",
     password="Empresa2026$",
-    database="empresa2026",
-    charset="utf8mb4"
+    database="empresa2026"
 )
 
-with conn.cursor() as cursor:
-    cursor.execute("SELECT * FROM clientes")
-    for row in cursor.fetchall():
-        print(row)
+cursor = conn.cursor()
 
+cursor.execute("SELECT * FROM clientes")
+
+for row in cursor.fetchall():
+    print(row)
+
+cursor.close()
 conn.close()
-
