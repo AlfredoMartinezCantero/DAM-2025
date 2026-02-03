bdb7f1302010609ece2551ab0926d7992f64dbf6
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2026-02-03 11:04:55 +0100

    Update index.php

--- PROGRAMACION/3er Trimestre/TEMA 10-Programación en el lado del servidor/016-Proyecto camarón/002-proceso formulario/index.php
+++ PROGRAMACION/3er Trimestre/TEMA 10-Programación en el lado del servidor/016-Proyecto camarón/002-proceso formulario/index.php
@@ -8,23 +8,53 @@
   	<header><h1>Camarón viviendas</h1></header>
     <nav>
     	<form action="?" method="POST">
-      	<select>
-            <option>Selecciona una localidad...</option>
-            <option value="Valencia">Valencia</option>
-            <option value="Alboraya">Alboraya</option>
-            <option value="Torrent">Torrent</option>
-            <option value="Gandía">Gandía</option>
-            <option value="Sagunto">Sagunto</option>
-            <option value="Paterna">Paterna</option>
-            <option value="Burjassot">Burjassot</option>
-            <option value="Xàtiva">Xàtiva</option>
-            <option value="Cullera">Cullera</option>
+      	<select name="localidad">
+        	<option>Selecciona una localidad...</option>
+          <option value="Valencia">Valencia</option>
+          <option value="Alboraya">Alboraya</option>
+          <option value="Torrent">Torrent</option>
+          <option value="Gandía">Gandía</option>
+          <option value="Sagunto">Sagunto</option>
+          <option value="Paterna">Paterna</option>
+          <option value="Burjassot">Burjassot</option>
+          <option value="Xàtiva">Xàtiva</option>
+          <option value="Cullera">Cullera</option>
         </select>
         <input type="submit">
       </form>
     </nav>
     <main>
-    	<section></section>
+    	<section>
+      	<?php
+          $host = "localhost";
+          $user = "camaron";
+          $pass = "Camaron123$";
+          $db   = "camaron";
+
+          $conexion = new mysqli($host, $user, $pass, $db);
+          $resultado = $conexion->query("
+            SELECT * FROM viviendas WHERE localidad = '".$_POST['localidad']."';
+          ");
+          while ($fila = $resultado->fetch_assoc()) {
+            echo '
+            	<article>
+              	<h3>'.$fila['localidad'].'</h3>
+                <p>'.$fila['precio'].'</p>
+                <p>'.$fila['metroscuadrados'].'</p>
+                <p>'.$fila['aniodeconstruccion'].'</p>
+                <p>'.$fila['direccion'].'</p>
+                <p>'.$fila['altura'].'</p>
+                <p>'.$fila['tipodevivienda'].'</p>
+                <p>'.$fila['descripcion'].'</p>
+                <p>'.$fila['estado'].'</p>
+                <p>'.$fila['banios'].'</p>
+                <p>'.$fila['habitaciones'].'</p>
+                <p>'.$fila['teniente'].'</p>
+              </article>
+            ';
+          }
+    		?>
+      </section>
     </main>
     <footer>(c) 2026 No me puedo creer que este proyecto se llame Camarón</footer>
   </body>
