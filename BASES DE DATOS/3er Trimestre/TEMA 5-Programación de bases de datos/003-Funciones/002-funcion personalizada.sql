35620592ebf93312047b9133335dca1122fed1d2
= <alfredomartinezcantero@gmail.com>
2026-02-25 09:33:54 +0100

    Update 002-funcion personalizada.sql

--- BASES DE DATOS/3er Trimestre/TEMA 5-Programación de bases de datos/003-Funciones/002-funcion personalizada.sql
+++ BASES DE DATOS/3er Trimestre/TEMA 5-Programación de bases de datos/003-Funciones/002-funcion personalizada.sql
@@ -1,7 +1,26 @@
-INSERT INTO clientes VALUES(
-	NULL,
-  CONCAT("Alfredo"),
-  CONCAT("Martinez"," ","Cantero"),
-  "alfredo@gmail.com",
-  "La calle de Alfredo"
+DELIMITER //
+
+CREATE FUNCTION nombre_completo(
+    p_nombre VARCHAR(255),
+    p_apellidos VARCHAR(255)
+)
+RETURNS VARCHAR(500)
+DETERMINISTIC
+BEGIN
+    RETURN CONCAT(p_nombre,' ',p_apellidos);
+END //
+
+DELIMITER ;
+
+INSERT INTO clientes(
+    nombre,
+    apellidos,
+    email,
+    direccion
+)
+VALUES(
+    nombre_completo('Alfredo','Martinez Cantero'),
+    'Martinez Cantero',
+    'alfredo@gmail.com',
+    'La calle de Alfredo'
 );
