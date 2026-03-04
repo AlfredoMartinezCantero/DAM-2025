8807fdd2bdd2b3f703a5d3830ceaa3cd744826e7
= <alfredomartinezcantero@gmail.com>
2026-03-04 09:31:27 +0100

    1

--- PROGRAMACION/3er Trimestre/TEMA 15-Cy Cpp/001-C/010-comparacion.c
+++ PROGRAMACION/3er Trimestre/TEMA 15-Cy Cpp/001-C/010-comparacion.c
@@ -3,11 +3,21 @@
 int main(){
 	int menorque = 4<3;
 	printf("%i",menorque);
+  printf("\n");
   int mayorque = 4>3;
 	printf("%i",mayorque);
-  int menoroigual = 4<= 3;
+  printf("\n");
+  int menoroigual = 4<=3;
 	printf("%i",menoroigual);
+  printf("\n");
   int mayorogigual = 4>=3;
-	printf("%i",mayorque);
+	printf("%i",mayorogigual);
+  printf("\n");
+  int igualdad = 4==3;
+	printf("%i",igualdad);
+  printf("\n");
+  int desigualdad = 4!=3;
+	printf("%i",desigualdad);
+  printf("\n");
   return 1;
 }
