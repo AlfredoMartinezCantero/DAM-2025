c655cb41077984e9ea29893e6e93632c0fc677a6
= <alfredomartinezcantero@gmail.com>
2026-03-09 09:21:40 +0100

    1

---
+++ PROGRAMACION/3er Trimestre/TEMA 12-Inteligencia Artificial/011-Agente IA/007-agente tonto.py
@@ -0,0 +1,3 @@
+archivo = open("/home/alfredo/Documentos/GitHub/DAM-2025/PROGRAMACION/3er Trimestre/TEMA 12-Inteligencia Artificial/011-Agente IA/agente.txt",'a')
+archivo.write("Soy un agente, pero un poco tonto\n")
+archivo.close()
---
+++ PROGRAMACION/3er Trimestre/TEMA 12-Inteligencia Artificial/011-Agente IA/008-ahora creo cron.md
@@ -0,0 +1,3 @@
+crontan -e
+
+* * * * * /usr/bin/python3 /home/alfredo/Documentos/GitHub/DAM-2025/PROGRAMACION/3er Trimestre/TEMA 12-Inteligencia Artificial/011-Agente IA/007-agente tonto.py
